d5e447302dba626d2ec587a7869db1f7c55c4c62
rafael <rafaelllparaynooo23@gmail.com>
2021-09-17 12:58:48 +0800

    first

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <div class="bgImg bgImg">
+
+
+        <div class="login-box">
+            <h1>LOGIN</h1>
+            <div class="textbox">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <input type="text" placeholder="Username" name="" value="">
+            </div>
+
+            <div class="textbox">
+                <i class="fa fa-lock" aria-hidden="true"></i>
+                <input type="password" placeholder="Password" name="" value="">
+            </div>
+
+            <button type="submit" class="btn">Login</button>
+        </div>
+    </div>
+</body>
+
+</html>
